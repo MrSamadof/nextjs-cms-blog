42c3601c6ca7ab57494736d4e784d30878f45690
MrSamadof <samadovnur@gmail.com>
2026-05-28 16:44:08 +0500

    fix: add null guards to BlogCard for image, author, tag, and category fields

--- components/cards/blog.tsx
+++ components/cards/blog.tsx
@@ -8,6 +8,8 @@ import { Badge } from '../ui/badge'
 import { cn, getReadingTime } from '@/lib/utils'
 import { format } from 'date-fns'
 
+const FALLBACK_IMAGE = 'https://us-west-2.graphassets.com/cmgfe2kkj071x07n6dup74m4b/cmgpavuhj7r2907n8bedqoubp'
+
 interface Props extends IBlog {
 	isVertical?: boolean
 }
@@ -26,15 +28,13 @@ function BlogCard(blog: Props) {
 					<Image
 						width={650}
 						height={335}
-						src={blog.image.url}
+						src={blog.image?.url ?? FALLBACK_IMAGE}
 						alt={blog.title}
 						className=' px-2 md:px-7 rounded-md group-hover:-translate-y-7 -translate-y-6 transition-all object-cover grayscale group-hover:grayscale-0 max-md:-translate-y-2 max-md:group-hover:-translate-y-3'
 					/>
 				</div>
 			</Link>
-			{/* Rasm (birinchi kolonka) */}
 
-			{/* Matnlar (ikkinchi kolonka) */}
 			<div className='flex flex-col space-y-4'>
 				{/* Time Info */}
 
@@ -47,7 +47,7 @@ function BlogCard(blog: Props) {
 						<Minus />
 						<div className='flex items-center gap-2'>
 							<Clock className='w-5 h-5' />
-							<p>{getReadingTime(blog.content.html)} min read</p>
+							<p>{getReadingTime(blog.content?.html ?? '')} min read</p>
 						</div>
 					</div>
 
@@ -62,33 +62,44 @@ function BlogCard(blog: Props) {
 					</p>
 				</Link>
 
-				{/* Author */}
+				{/* Author + meta */}
 				<div className='flex items-center gap-4'>
-					<div className=' flex items-center gap-2'>
-						<Image
-							src={blog.author.image.url}
-							alt='Author'
-							width={30}
-							height={30}
-							className='mt-4 rounded-md'
-						/>
-						<p>by{blog.author.name}</p>
-					</div>
-					<Dot />
-					<div className='flex items-center gap-2'>
-						<Link href={`/tags/${blog.tag.slug}`}>
-							<Badge variant={'secondary'} role='button'>
-								<Tag className='w-3 h-3 me-2'/>
-								{blog.tag.name}
-							</Badge>
-						</Link>
-												<Link href={`/categories/${blog.category.slug}`}>
-							<Badge variant={'outline'} role='button'>
-								<Layers2 className='w-3 h-3 me-2'/>
-								{blog.category.name}
-							</Badge>
-						</Link>
-					</div>
+					{blog.author && (
+						<div className='flex items-center gap-2'>
+							<Image
+								src={blog.author.image?.url ?? FALLBACK_IMAGE}
+								alt='Author'
+								width={30}
+								height={30}
+								className='mt-4 rounded-md'
+							/>
+							<p>by {blog.author.name}</p>
+						</div>
+					)}
+
+					{(blog.tag || blog.category) && (
+						<>
+							{blog.author && <Dot />}
+							<div className='flex items-center gap-2'>
+								{blog.tag && (
+									<Link href={`/tags/${blog.tag.slug}`}>
+										<Badge variant={'secondary'} role='button'>
+											<Tag className='w-3 h-3 me-2'/>
+											{blog.tag.name}
+										</Badge>
+									</Link>
+								)}
+								{blog.category && (
+									<Link href={`/categories/${blog.category.slug}`}>
+										<Badge variant={'outline'} role='button'>
+											<Layers2 className='w-3 h-3 me-2'/>
+											{blog.category.name}
+										</Badge>
+									</Link>
+								)}
+							</div>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
